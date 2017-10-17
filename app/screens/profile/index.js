d100534e794c5b8cae15cc0f41710975a44eaf7f
Adam Adams <ajadams10@gmail.com>
2017-10-16 20:45:40 -0700

    styling profile page and adding profile pic and name

--- app/screens/profile/index.js
+++ app/screens/profile/index.js
@@ -1,18 +1,60 @@
 // /screens/profile/index.js
 
 // libs
+import { connect } from 'react-redux'
 import React, { Component } from 'react'
+import * as Animatable from 'react-native-animatable'
+import Icon from 'react-native-vector-icons/FontAwesome'
 import {
 	View,
 	Text,
+	Image,
 	TouchableOpacity,
 } from 'react-native'
 
 // styles
 import { pro } from './../../styles/profile'
+import { darkTheme } from './../../styles/_global'
 
-export default () => (
-	<View style={pro.container}>
-		<Text>In profile</Text>
-	</View>
-)
+class Profile extends Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		const { name, picture } = this.props._user;
+
+		return (
+			<View style={pro.container}>
+
+				<View style={pro.header}>
+					<TouchableOpacity style={pro.headBtn}>
+						<Icon name="user-plus" color={darkTheme.shade3} size={20} />
+					</TouchableOpacity>
+
+					<Text style={pro.headTitle}>Profile</Text>
+
+					<TouchableOpacity style={pro.headBtn}>
+						<Icon name="cogs" color={darkTheme.shade3} size={20} />
+					</TouchableOpacity>
+				</View>
+
+				<View style={pro.main}>
+					<View style={pro.profilePic}>
+						<Image source={{uri: picture.data.url}} style={pro.pic} />
+					</View>
+					<Text style={pro.name}>{ name }</Text>
+				</View>
+
+			</View>
+		);
+	}
+}
+
+const mapStateToProps = (state, props) => {
+	return {
+		_user: state._user,
+	}
+}
+
+export default connect(mapStateToProps)(Profile);
--- app/styles/profile.js
+++ app/styles/profile.js
@@ -11,6 +11,44 @@ export const pro = StyleSheet.create({
 		justifyContent: 'space-around',
 		alignItems: 'center',
 		backgroundColor: _g.darkTheme.shade5,
-		..._g._border(1, 'red'),
+		// ..._g._border(1, 'red'),
 	},
+	header: {
+		height: _g.header_height,
+		alignItems: 'flex-end',
+		backgroundColor: 'transparent',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		..._g._padding(0, 15, 0, 15),
+		height: _g.header_height,
+		// ..._g._border(1, 'red'),
+		width: '100%',
+		position: 'absolute',
+		top: 0,
+	},
+	headTitle: {
+		color: _g.darkTheme.shade3,
+		fontSize: 20,
+		fontWeight: '500',
+	},
+	main: {
+		// ..._g._border(1,'red'),
+		alignItems: 'center',
+	},
+	profilePic: {
+		width: 60,
+		height: 60,
+		overflow: 'hidden',
+		..._g._borderRadius(5),
+	},
+	pic: {
+		width: '100%',
+		height: '100%',
+		resizeMode: 'contain',
+	},
+	name: {
+		color: _g.darkTheme.shade3,
+		fontSize: 20,
+		marginTop: 10,
+	}
 });
